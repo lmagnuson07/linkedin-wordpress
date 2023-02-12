c4cb54315370c9b15010e8c3d0c302a416a8226c
lmagnuson07 <logmagns07@gmail.com>
2023-02-11 23:22:14 -0700

    Uninstallation methods added. Using the unistall.php method.

---
+++ wp-content/plugins/myplugin/uninstall.php
@@ -0,0 +1,28 @@
+<?php
+/*
+
+	uninstall.php
+
+	- fires when plugin is uninstalled via the Plugins screen
+
+*/
+
+
+
+// exit if uninstall constant is not defined. Protects against direct access.
+// will be defined during the uninstall.php invocation
+// is not defined when register_uninstall_hook is used.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+
+    exit;
+
+}
+// delete the plugin options
+delete_option( 'myplugin_options' );
+
+// for site options in Multisite
+//      delete_site_option( $option_name );
+
+// drop a custom database table
+//      global $wpdb;
+//      $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}mytable" );
